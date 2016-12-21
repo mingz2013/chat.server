22d9f6b67dc131736aa2587d6c084297fcee246f
mingz <305603665@qq.com>
2016-12-21 21:00:58 +0800

    init

--- app/namespaces/ChatNamespace.py
+++ app/namespaces/ChatNamespace.py
@@ -6,11 +6,53 @@ thread = None
 
 
 class ChatNamespace(Namespace):
+    def on_connect(self):
+        print "connect id", request.sid
+        # session['receive_count'] = session.get('receive_count', 0) + 1
+        # global thread
+        # if thread is None:
+        # thread = self.socketio.start_background_task(target=self.background_thread)
+        # emit('heartbeat', {'data': 'Connected', 'count': session['receive_count']})
+
+    def on_signin(self, message):
+        current_app.logger.info(message)
+        auth = message.get('auth')
+        if auth.get('token'):
+            emit(message.get('cmd'), {"retcode": 0, "result": ""})
+        else:
+            emit(message.get('cmd'), {"retcode": -1, "result": "", "errmsg": "not found token"})
+
+    def on_register(self, message):
+        current_app.logger.info(message)
+        auth = message.get('auth')
+        data = message.get('data')
+        if not data:
+            emit(message.get('cmd'), {"retcode": -1, "result": "", "errmsg": "not found data"})
+            return
+        username = data.get("username")
+        password = data.get("password")
+        if not username or not password:
+            emit(message.get('cmd'), {"retcode": -1, "result": "", "errmsg": "not found username or password"})
+            return
+        token = username + password
+        auth = {"token": token}
+        emit(message.get('cmd'), {"retcode": 0, "result": auth, "errmsg": ""})
+
     def on_login(self, message):
         current_app.logger.info(message)
-        session['receive_count'] = session.get('receive_count', 0) + 1
-        emit('my_response',
-             {'data': message['data'], 'count': session['receive_count']})
+        auth = message.get('auth')
+        data = message.get('data')
+        if not data:
+            emit(message.get('cmd'), {"retcode": -1, "result": "", "errmsg": "not found data"})
+            return
+        username = data.get("username")
+        password = data.get("password")
+        if not username or not password:
+            emit(message.get('cmd'), {"retcode": -1, "result": "", "errmsg": "not found username or password"})
+            return
+        token = username + password
+        auth = {"token": token}
+        emit(message.get('cmd'), {"retcode": 0, "result": auth, "errmsg": ""})
 
     def on_my_event(self, message):
         print message
@@ -60,14 +102,7 @@ class ChatNamespace(Namespace):
     def on_my_ping(self):
         emit('my_pong')
 
-    def on_connect(self):
-        print "connect id", request.sid
-        session['receive_count'] = session.get('receive_count', 0) + 1
 
-        # global thread
-        # if thread is None:
-        # thread = self.socketio.start_background_task(target=self.background_thread)
-        emit('heartbeat', {'data': 'Connected', 'count': session['receive_count']})
 
     def on_disconnect(self):
         print('Client disconnected', request.sid)
