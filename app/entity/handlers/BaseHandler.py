daa8ab5e69253a950c3e7e679740688293ea3715
mingz <305603665@qq.com>
2016-12-23 22:43:29 +0800

    init

---
+++ app/entity/handlers/BaseHandler.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+__author__ = 'zhaojm'
+
+
+class BaseHandler(object):
+    def __init__(self):
+        pass
+
+    def handle(self, cmd, data=None):
+        pass
--- app/entity/namespaces/ChatNamespace.py
+++ app/entity/namespaces/ChatNamespace.py
@@ -9,22 +9,29 @@ class ChatNamespace(Namespace):
     def on_disconnect(self):
         print 'on disconnect', request.sid
 
-    def on_message(self, message):
-        print "on message..."
-        print message
-
-    def on_anything(self, message):
-        print "on anything..."
-        print message
-
-    def on_json(self, json):
-        print "on json..."
-        print json
-
     def on_error(self, e):
         print "on error"
         print e
 
-    def on_my_event(self, message):
-        print "on my event"
+    def on_message(self, message):  # send
+        print "on message..."
         print message
+        cmd = message.get("cmd")
+        data = message.get("data")
+        print cmd
+        print data
+
+
+
+
+        # def on_anything(self, message):
+        #     print "on anything..."
+        #     print message
+        #
+        # def on_json(self, json):
+        #     print "on json..."
+        #     print json
+        #
+        # def on_my_event(self, message):
+        #     print "on my event"
+        #     print message
