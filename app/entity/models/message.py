9e77773d9cf31ed2a8989077c8a7d2101a9e4e64
mingz <305603665@qq.com>
2016-12-25 15:53:41 +0800

    init

--- app/entity/models/message.py
+++ app/entity/models/message.py
@@ -7,12 +7,5 @@ from app import db
 class Message(db.Document):
     send_time = db.DateTimeField()
     content = db.StringField()
-
-
-class ChatMessage(Message):
-    room = db.ObjectIdField()
     send_from = db.ObjectIdField()
 
-
-class SystemMessage(Message):
-    pass
