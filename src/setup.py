4943256b38ad3b79d805b756b43fe6f5bc62e22e
mingz <305603665@qq.com>
2017-03-29 14:30:34 +0800

    init

--- src/setup.py
+++ src/setup.py
@@ -3,6 +3,9 @@
 import os
 import sys
 
+# current_path = os.path.dirname(os.path.abspath(__file__))
+# source_path = os.path.join(current_path, "../../")
+# sys.path.append(os.path.join(source_path, "difang/src/"))
 # sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.abspath('.'))
 
@@ -13,7 +16,7 @@ if __name__ == '__main__':
     init_encoding()
 
     from app import Server
-    from app.webapp import webapp
+    from webapp import webapp
 
     print "run on 0.0.0.0:5000"
     Server().run(webapp, host="0.0.0.0", port=5000, reload=True)
